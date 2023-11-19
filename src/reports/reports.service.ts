5d2ff840a9aae120e6d93df6eff3539582bee8e5
Marcin <wanago.marcin@gmail.com>
2023-11-19 05:43:23 +0100

    feat(Reports): get the length of the longest and shortest articles

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,18 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
+import { isRecord } from '../utilities/is-record';
 
 @Injectable()
 export class ReportsService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  private async getArticlesLength() {
+    const rawResults = await this.prismaService.$queryRaw`
+      SELECT
+      max(length(content)) AS longest_article_length,
+      min(length(content)) AS shortest_article_length
+      FROM "Article"
+    `;
+    if (!Array.isArray(rawResults)) {
+      throw new InternalServerErrorException();
+    }
+    const result = rawResults[0];
+    if (!isRecord(result)) {
+      throw new InternalServerErrorException();
+    }
+    const longestArticleLength = result.longest_article_length;
+    const shortestArticleLength = result.shortest_article_length;
+    return {
+      longestArticleLength,
+      shortestArticleLength,
+    };
+  }
 
   async getArticlesStatistics() {
     const result = await this.prismaService.article.aggregate({
       _avg: {
         upvotes: true,
       },
+      _max: {
+        upvotes: true,
+      },
     });
+
+    const { longestArticleLength, shortestArticleLength } =
+      await this.getArticlesLength();
+
     return {
-      averageUpvotesCount: result._avg.upvotes
-    }
+      averageUpvotesCount: result._avg.upvotes,
+      biggestUpvotesCount: result._max.upvotes,
+      longestArticleLength,
+      shortestArticleLength,
+    };
   }
 }
