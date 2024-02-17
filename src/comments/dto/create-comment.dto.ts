84f678509c1c1cb34d1d4a0ca11b525afec955b1
Marcin <wanago.marcin@gmail.com>
2024-02-18 00:31:08 +0100

    feat(Comments): add the ContainsValidForeignKeys decorator

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,5 +1,47 @@
+import {
+  IsNotEmpty,
+  IsString,
+  registerDecorator,
+  ValidationArguments,
+} from 'class-validator';
+
+const idKeys: (keyof CreateCommentDto)[] = ['photoId', 'articleId'];
+
+export function ContainsValidForeignKeys() {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'containsValidForeignKeys',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: {
+        message: `You need to provide exactly one of the following properties: ${idKeys.join(', ',)}`,
+      },
+      validator: {
+        validate(value: unknown, validationArguments: ValidationArguments) {
+          const comment = validationArguments.object as CreateCommentDto;
+
+          if (value && !Number.isInteger(value)) {
+            return false;
+          }
+
+          return (
+            !idKeys.every((key) => comment[key]) &&
+            idKeys.some((key) => comment[key])
+          );
+        },
+      },
+    });
+  };
+}
+
 export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @ContainsValidForeignKeys()
   photoId?: number;
+
+  @ContainsValidForeignKeys()
   articleId?: number;
 }
